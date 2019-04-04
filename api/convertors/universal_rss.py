fc91ce20680376fb5d657edc3907b1725fe73b1a
w4pity <w4pity@gmail.com>
2019-04-04 02:08:13 +0200

    remove thread

--- api/convertors/universal_rss.py
+++ api/convertors/universal_rss.py
@@ -36,7 +36,7 @@ class UniversalRss:
         for feed in feeds:
             news_feed = feedparser.parse(feed.url)
             for rss in news_feed.entries:
-                r = Rss.objects.create(channel_id=feed.id,
+                r, _ = Rss.objects.create(channel_id=feed.id,
                                        title=rss.get('title', None),
                                        description=rss.get('summary', None),
                                        url_image=rss.get('r', None),
@@ -45,6 +45,8 @@ class UniversalRss:
 
                 if request.user.is_anonymous or len(RssStatus.objects.filter(user=request.user, rss=r)) == 0:
                     rss_list.append(r)
+                else:
+                    pass
         # if not isinstance(user, AnonymousUser):
         #     rss.user_read.add(user)
         return sorted(rss_list, key=lambda x: x.date, reverse=True)[:20]
