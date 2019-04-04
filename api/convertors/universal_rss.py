998427ca16a00a50219af25eda9400f28e7ec4c7
Melvyn Petrocchi <mpetrocchi@ennov.com>
2019-04-04 12:20:29 +0200

    Fix data duplication

--- api/convertors/universal_rss.py
+++ api/convertors/universal_rss.py
@@ -28,22 +28,26 @@ class UniversalRss:
             chs = [ch.id for ch in Channel.objects.filter(Q(user=None) | Q(user=request.user))]
             feeds = list(Channel.objects.filter(pk__in=[ch for ch in chs if ch not in blchs]))
         else:
-            feeds = list(Channel.objects.filter(user=None))[:1]
+            feeds = list(Channel.objects.filter(user=None))
 
         rss_list = []
 
         for feed in feeds:
             news_feed = feedparser.parse(feed.url)
             for rss in news_feed.entries[:8]:
-                r, _ = Rss.objects.get_or_create(channel_id=feed.id,
-                                          title=rss.get('title', None),
-                                          description=rss.get('summary', None),
-                                          url_image=rss.get('r', None),
-                                          url_origin=rss.get('link', None),
-                                          date=self.get_date(rss.get('published_parsed', None)))
-                print(r.id)
+                try:
+                    r = Rss.objects.get(url_origin=rss.get('link', None))
+                except:
+                    r = Rss.objects.create(channel_id=feed.id,
+                                           title=rss.get('title', None),
+                                           description=rss.get('summary', None),
+                                           url_image=rss.get('r', None),
+                                           url_origin=rss.get('link', None),
+                                           date=self.get_date(rss.get('published_parsed', None)))
+
                 if request.user.is_anonymous or len(RssStatus.objects.filter(user=request.user, rss=r)) == 0:
-                    rss_list.append(r)
+                    if not next((x for x in rss_list if x.url_origin == rss.get('link', None)), None):
+                        rss_list.append(r)
         # if not isinstance(user, AnonymousUser):
         #     rss.user_read.add(user)
         return sorted(rss_list, key=lambda x: x.date, reverse=True)
